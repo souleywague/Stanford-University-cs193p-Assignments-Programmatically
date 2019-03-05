b25c94cc858119a0124c86626ece36cf2d1f606a
souley_wague <souleymane.wague@gmail.com>
2019-03-05 12:54:56 +0200

    Setup SetViewController

---
+++ Programming Project 2 - Set/Programming Project 2 - Set/SetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SetViewController.swift
+//  Programming Project 2 - Set
+//
+//  Created by Souley on 05/03/2019.
+//  Copyright © 2019 Souley. All rights reserved.
+//
+
+import UIKit
+
+class SetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
